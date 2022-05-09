49d72097e4632d91553fc5f65269ccc9dcf49203
김솔 <rlathfrlathf@gmail.com>
2022-05-10 02:36:16 +0900

    지금까지 결과

--- app/src/main/java/com/example/teamproject2/PagerAdapter.java
+++ app/src/main/java/com/example/teamproject2/PagerAdapter.java
@@ -4,40 +4,65 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-public class PagerAdapter extends FragmentStateAdapter {
-    private static int NUM_ITEMS=3;
-    private int POS;
-    MonthCalc mva = new MonthCalc();
+import java.util.ArrayList;
 
+public class PagerAdapter extends FragmentStateAdapter {
+    //private static int NUM_ITEMS=3;
+    private int START_POS =10;
+
+    MonthCalc mva = new MonthCalc();
     public PagerAdapter(FragmentActivity fa) {
         super(fa);
     }
-
     @Override
-    public long getItemId(int position) {
+    public long getItemId(int position)
+    {
         int[] current = mva.calcCal();
+        if(position ==START_POS)
+            return (current[0]*10000+current[1]*100+current[2]);
+        System.out.println("---------------------------------------------------------------current[0]"+current[0]);
+        System.out.println("---------------------------------------------------------------position"+position);
+
+        int move = position - START_POS;
+        System.out.println("---------------------------------------------------------------move="+move);
+        //if(move==0) return (current[0]*10000+current[1]*100+current[2]);
+        if(current[1]+move<1)
+        {
+            current[0]--;
+            current[1]=current[1]+move+12;
+        }
+        else if(current[1]+move>12)
+        {
+            current[0]++;
+            current[1]=current[1]+move-12;
+        }
+        else
+        {
+            current[1]+=move;
+        }
 
         return (current[0]*10000+current[1]*100+current[2]);
     }
 
     // 각 페이지를 나타내는 프래그먼트 반환
-    @Override
 
+    //ArrayList<Fragment> pages = new ArrayList<Fragment>();
+    @Override
     public Fragment createFragment(int position)
     {
-        long current = getItemId(position);
+        long itemID = getItemId(position);
         contentFragment fg = new contentFragment();
         Bundle args = new Bundle();
-        args.putLong("yearMonth",current);
+        args.putLong("yearMonth",itemID);
         fg.setArguments(args);
-        System.out.println("-------------------------------------------------------------------bundle 전달 전 current값: "+current);
         return fg;
     }
 
     @Override
     public int getItemCount() {
-        return NUM_ITEMS;
+        return 20;
     }
 }
