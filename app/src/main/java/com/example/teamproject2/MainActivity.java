5e63809b07d968646cd7397c6931a0526e812568
Jung seung hwan <pilot044@naver.com>
2022-05-10 02:09:54 +0900

    Revert "1"
    This reverts commit 4778f23f06258739c0b7a6e0e22c961ac21ab6fe, reversing
    changes made to 1c87b92cbb6a66eb8f30428124643e265fc46952.

--- app/src/main/java/com/example/teamproject2/MainActivity.java
+++ app/src/main/java/com/example/teamproject2/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.teamproject2;
 
 import androidx.appcompat.app.AppCompatActivity;
 <<<<<<< HEAD
+<<<<<<< HEAD
 import androidx.fragment.app.Fragment;
 =======
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 >>>>>>> 9846d0ce794a44cfd33c3723d6fd26dedc1de22c
+=======
+>>>>>>> parent of 9846d0c (Viewpager2)
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -25,23 +28,29 @@ public class MainActivity extends AppCompatActivity implements contentFragment.f
     public static int gridviewWidth, gridviewHeight;
     public static Context mContext;
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
     private ViewPager2 vpPager;
     //PagerAdapter fragmentViewPagerAdapter;
 >>>>>>> 9846d0ce794a44cfd33c3723d6fd26dedc1de22c
+=======
+>>>>>>> parent of 9846d0c (Viewpager2)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mContext = this;
+        TextView nowDate;
+        nowDate = (TextView)findViewById(R.id.YearMonth);
 
-        vpPager = findViewById(R.id.vpPager);
+        //System.out.println("-------------------------------------------------------------------Main current[0]="+current[0]+" [1]="+current[1]);
+        // System.out.println("------------------------------------------------------------------호출됨?------------------");
+        mContext = this;
+        ViewPager2 vpPager = findViewById(R.id.vpPager);
         FragmentStateAdapter adapter = new PagerAdapter(this);
         vpPager.setAdapter(adapter);
-        vpPager.setCurrentItem(10,false);
-
-        TextView nowDate = (TextView) findViewById(R.id.YearMonth);
         nowDate.setText(current[0] + "년 " + current[1] + "월");
+        System.out.println("-------------------------------------------------------------------Main current[0]="+current[0]+" [1]="+current[1]);
+
     }
 
     public void onDateSelected(String year, String month, String date) {
@@ -55,13 +64,14 @@ public class MainActivity extends AppCompatActivity implements contentFragment.f
         current[0] = year;
         current[1] = month;
         current[2] = day;
-        TextView nowDate = (TextView) findViewById(R.id.YearMonth);
-        nowDate.setText(current[0] + "년 " + current[1] + "월");
+        TextView nowdate = (TextView) findViewById(R.id.YearMonth);
+        nowdate.setText(current[0] + "년 " + current[1] + "월");
     }
 
 
     @Override
     public void mainGetDisplay(int w, int h) {
+        System.out.println("-------------------------------------------------------------------w="+w+" h="+h);
 
         gridviewWidth = w;
         gridviewHeight = h;
